9fc388b88c0835cec951a676cb0cd1c602a56757
Stino <Linksonder@gmail.com>
2016-09-03 17:31:39 +0200

    typescript werkt, maar builden is lastig

--- src/Webdictaat.Web/gulpfile.js
+++ src/Webdictaat.Web/gulpfile.js
@@ -6,7 +6,7 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var typescript = require('gulp-tsc');
 
 var paths = {
     webroot: "./wwwroot/",
@@ -18,7 +18,32 @@ gulp.task('sass', function () {
       .pipe(gulp.dest(paths.webroot + 'css'));
 });
 
+
+
 gulp.task('sass:watch', function () {
     gulp.watch(paths.webroot + 'sass/**/*.scss', ['sass']);
+});
+
+gulp.task('copy:angular_dependencies', function () {
+    gulp.src([
+        './node_modules/core-js/client/shim.min.js',
+        './node_modules/zone.js/dist/zone.js',
+        './node_modules/reflect-metadata/Reflect.js',
+        './node_modules/systemjs/dist/system.src.js',
+    ])
+    // Perform minification tasks, etc here
+    .pipe(gulp.dest(paths.webroot + 'lib/angular_dependencies'));
+});
+
+gulp.task('copy:required_angular_modules', function () {
+    gulp.src('./node_modules/@angular/**/*')
+    // Perform minification tasks, etc here
+    .pipe(gulp.dest(paths.webroot + 'node_modules/@angular/'));
+});
+
+gulp.task('copy:required_rxjs_modules', function () {
+    gulp.src('./node_modules/rxjs/**/*')
+    // Perform minification tasks, etc here
+    .pipe(gulp.dest(paths.webroot + 'node_modules/rxjs/'));
 });
 
