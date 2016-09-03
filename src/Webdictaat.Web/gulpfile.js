46e2a11f12173fa4cddfdb37911ebe4b2a15b110
Stino <Linksonder@gmail.com>
2016-09-03 16:14:23 +0200

    First steps of adding angular

---
+++ src/Webdictaat.Web/gulpfile.js
@@ -0,0 +1,24 @@
+﻿/// <binding AfterBuild='sass' ProjectOpened='sass:watch' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+var paths = {
+    webroot: "./wwwroot/",
+};
+
+gulp.task('sass', function () {
+    return gulp.src(paths.webroot + 'sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest(paths.webroot + 'css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch(paths.webroot + 'sass/**/*.scss', ['sass']);
+});
+
---
+++ src/Webdictaat.Web/readme.md
@@ -0,0 +1,15 @@
+﻿
+Hoi Stijn
+
+Even herinneren waar je was:
+
+* Los alle bower en npm en gulp dingen verwijders
+* een voor een weer toegevoegd
+* Nu alleen een SASS task in gulp en jquery en bootstrap via bower
+* je kan geen angular2 installeren via bower, is kapot
+
+	Volgende stap:
+	Hoe ga je angular2 installeren? 
+
+	What else?
+	Wellicht kijken in hoeverre bower helemaal niet meer nodig is in het project.
